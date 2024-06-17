4a1cff6d5fd7267063e3e5533b276723de1d14f7
ECCC-CSSP <charles.leblanc2@canada.ca>
2024-06-17 16:40:43 -0300

    update

--- CSSPLabSheet/Models/CSSPLabSheetDBEDM.Designer.cs
+++ CSSPLabSheet/Models/CSSPLabSheetDBEDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\CSSP Latest Code\CSSPLabSheet\CSSPLabSheet\Models\CSSPLabSheetDBEDM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\CSSP Latest Code Old\CSSPLabSheet\CSSPLabSheet\Models\CSSPLabSheetDBEDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
